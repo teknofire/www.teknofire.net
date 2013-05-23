dc6a3cf5794470479fc78e11ac81b7ac862c1b71
Will Fisher <will@teknofire.net>
2013-05-23 11:11:00 -0500

    fixing sub page and adding notes

--- Gemfile
+++ Gemfile
@@ -9,6 +9,7 @@ gem 'sqlite3'
 gem 'haml-rails'
 gem 'bootstrap-sass'
 gem 'font-awesome-rails'
+gem 'redcarpet'
 
 # Gems used only for assets and not required
 # in production environments by default.
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ TeknofireNet::Application.routes.draw do
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
+  resources :notes
+  
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
   # Keep in mind you can assign values other than :controller and :action
