5b2a215470bb0bf90229b3fd9743582f91a6cf4d
Will Fisher <will@teknofire.net>
2013-05-23 10:17:15 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TeknofireNet::Application.config.secret_token = '5e984534ed7e5bc672b6a0b797a8f8a68419aaa46d71e13dfdb1980d68ef65e132ee39b64da5678d633e749acfe493d1d502fcf3d2d6768ede0df2a435d01611'
