731728baa7cddde00bd2b8bd43c4cf8da5e7706f
Will Fisher <will@teknofire.net>
2013-05-25 11:15:47 -0500

    hooking up new notes

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,5 +1,6 @@
 class NotesController < ApplicationController
   def index
-    @notes = %w{ mobile_mapping_w_enyojs geojson_is_wrong postgis_frenzy }
+    @notes = %w{ mobile_mapping_w_enyojs geojson_is_wrong postgis_frenzy 
+      mongodb_geospatial high_performance_data_vis openlayers3 }
   end
 end
